566a74b95f370c6f70c0721ebfdc43ed0cbb06b1
Beth Noser <32307322+bethknowsher@users.noreply.github.com>
2019-10-25 12:53:34 -0500

    create var and function

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+var jokes = [
+  "Why was Santa's little helper feeling depressed? Because he has low elf esteem.",
+  "How are false teeth like stars? They come out at night!"
+  "What type of bees make milk? Boo-bees."
+  "Did you hear about the cow who jumped over the barbed wire fence? It was udder destruction."
 
+  function newJoke() {
+    var randomNumber = Math.floor(Math.random() * jokes.length));
+  
+  document.getElementById('jokeDisplay').innerHTML = jokes[randomNumber];
