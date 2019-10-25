eb448a8ace860f95140a1e2e3717645a3e994b2a
Beth Noser <32307322+bethknowsher@users.noreply.github.com>
2019-10-25 12:58:41 -0500

    add missing bracket

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var jokes = [
   "How are false teeth like stars? They come out at night!"
   "What type of bees make milk? Boo-bees."
   "Did you hear about the cow who jumped over the barbed wire fence? It was udder destruction."
+  ]
 
   function newJoke() {
     var randomNumber = Math.floor(Math.random() * jokes.length));
